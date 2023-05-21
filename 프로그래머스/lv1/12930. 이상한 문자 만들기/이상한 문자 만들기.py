8ea311752af3f0b16b10f60d5ad5620b3a6a7b35
Eunyoung.H <83502660+dazzlingHeyD@users.noreply.github.com>
2023-05-21 21:57:03 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,19 @@
+def solution(s):
+    ans = []
+    s = s.split(' ')
+    for i in range(len(s)):
+        res = ''
+        for j in range(len(s[i])):
+            if j % 2 == 0:
+                res += s[i][j].upper()
+            else:
+                res += s[i][j].lower()
+
+        ans.append(res)
+
+    return ' '.join(ans)
+
+            
+            
+            
+    
