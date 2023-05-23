9f8e5065fa4a6f2d44bf2db3cfd50d0d5b388cbb
Eunyoung.H <83502660+dazzlingHeyD@users.noreply.github.com>
2023-05-23 16:10:57 +0900

    Update K번째수.py

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -12,6 +12,15 @@ def solution(array, commands):
         
     return newlist
 
+
+
+
+
+2
+3# 다른 사람 풀이 
+# def solution(array, commands):
+#     return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
+
         
 
 
