b58d288cce1151bd2feff131439b351f4ab67231
Eunyoung.H <83502660+dazzlingHeyD@users.noreply.github.com>
2023-05-25 14:40:24 +0900

    [level 2] Title: 행렬의 곱셈, Time: 0.42 ms, Memory: 28.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,8 +1,4 @@
-def solution(arr1, arr2):
-    answer = [[0]*len(arr2[0]) for _ in range(len(arr1))]
-    for i in range(len(arr1)): 
-        lists = []
-        for j in range(len(arr2[0])): 
-            for k in range(len(arr1[0])): 
-                answer[i][j] += arr1[i][k] * arr2[k][j]
-    return answer
+import numpy as np
+def solution(A, B):
+
+    return (np.matrix(A)*np.matrix(B)).tolist()
